7bdcddf3974d78022cd4068cbfa8c0793c3be31b
ccmxy <windrainicefire@gmail.com>
2015-09-17 19:58:08 -0700

    added tests

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,17 @@
 
-describe('getFactorial', function() {
-  it("is 24 for factorial 4", function() {
-    expect(getFactorial(4)).to.equal(24);
+describe('bottlesOfBeer', function() {
+  it("0 bottles comes out right", function() {
+    expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall,"+
+    " no more bottles of beer. Go to the store and buy some more, "+
+    " 99 bottles of beer on the wall.");
+  });
+
+  it("2 bottles comes out right", function() {
+    expect(bottlesOfBeer(2)).to.equal("\n2 bottles of beer on the wall,"+
+    " 2 bottles of beeer.... take one down, pass it around,"+
+    "\n1 bottles of beer on the wall!\n1 bottles of beer on the wall,"+
+    " 1 bottles of beeer.... take one down, pass it around,\n0 bottles of beer on the wall!No more bottles of beer on the wall, no more bottles of beer."+
+    " Go to the store and buy some more,  99 bottles of beer on the wall.");
   });
 
 });
