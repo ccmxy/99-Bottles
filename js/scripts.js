fd2fd6cc2210e24baceabb95ca9bde02e35d42ee
ccmxy <windrainicefire@gmail.com>
2015-09-17 20:06:22 -0700

    all tests passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
 
-var bottlesOfBeer = function(number) {
-  var temp = ("\n" + number + " bottles of beer on the wall, " +
-  number + " bottles of beeer.... take one down, " +
-  "pass it around,\n" + (number - 1) +
-  " bottles of beer on the wall!");
-  temp = temp
-  if(number < 1){
-    return 1;
-  }
-  /*
-  temp = (temp + bottlesOfBeer(number - 1));
-  */
-  return temp;
-};
+var bottlesOfBeerForScripts = function(number) {
+    var temp = ("\n" + number + " bottles of beer on the wall, " +
+    number + " bottles of beeer.... take one down, " +
+    "pass it around,\n" + (number - 1) +
+    " bottles of beer on the wall!");
+    if(number < 1){
+      return ("No more bottles of beer on the wall," +
+       " no more bottles of beer." +
+       " Go to the store and buy some more,"  +
+     "  99 bottles of beer on the wall.");
+    }
+    temp = (temp + bottlesOfBeerForScripts(number - 1));
+    return temp;
+  };
 
 
 $(document).ready(function() {
   $("form#get-fact").submit(function(event) {
     var number = parseInt($("input#number").val());
-  //  var factorial = getFactorial(number);
-    var bottles = bottlesOfBeer(number);
-  /*  for(var i = 0; i < number; i++){
-      bottles = bottlesOfBeer(i);
-      $(".bottles").text(bottles);
-    }*/
-
     event.preventDefault();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,19 @@
 
-describe('bottlesOfBeer', function() {
+describe('bottlesOfBeerForScripts', function() {
   it("0 bottles comes out right", function() {
-    expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall,"+
+    expect(bottlesOfBeerForScripts(0)).to.equal("No more bottles of beer on the wall,"+
     " no more bottles of beer. Go to the store and buy some more, "+
     " 99 bottles of beer on the wall.");
   });
 
   it("2 bottles comes out right", function() {
-    expect(bottlesOfBeer(2)).to.equal("\n2 bottles of beer on the wall,"+
+    expect(bottlesOfBeerForScripts(2)).to.equal("\n2 bottles of beer on the wall,"+
     " 2 bottles of beeer.... take one down, pass it around,"+
     "\n1 bottles of beer on the wall!\n1 bottles of beer on the wall,"+
     " 1 bottles of beeer.... take one down, pass it around,\n0 bottles of beer on the wall!No more bottles of beer on the wall, no more bottles of beer."+
     " Go to the store and buy some more,  99 bottles of beer on the wall.");
   });
+
+  ///oissue
 
 });
