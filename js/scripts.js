f3bfb6a830c1762e3fed745d376dc2410468dcff
ccmxy <windrainicefire@gmail.com>
2015-09-17 19:18:19 -0700

    with recursion but string has no newlines

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,37 @@ var getFactorial = function(number) {
   return temp;
 };
 
+var bottlesOfBeer = function(number) {
+  var temp = ("\n" + number + " bottles of beer on the wall, " +
+  number + " bottles of beeer.... take one down, " +
+  "pass it around,\n" + (number - 1) +
+  " bottles of beer on the wall!");
+  temp = temp
+  if(number < 1){
+    return 1;
+  }
+  temp = (temp + bottlesOfBeer(number - 1));
+  return temp;
+};
+
+/*
+var bottlesOfBeer = function(number) {
+  var myString = (number + "bottles of beer on the wall")
+  console.log(number + " bottles of beer on the wall...");
+  if (number > 0) {
+    bottlesOfBeer(number - 1);
+  }
+  return myString;
+};
+*/
+
+
 $(document).ready(function() {
   $("form#get-fact").submit(function(event) {
     var number = parseInt($("input#number").val());
     var factorial = getFactorial(number);
+    var bottles = bottlesOfBeer(number);
+    $(".bottles").text(bottles);
     $(".number").text(number);
     $(".factorial").text(factorial);
     $('#theFactorial').show();
