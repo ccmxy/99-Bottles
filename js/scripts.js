8f862f1de7e7fefd63606c2d24fde0339cd30378
ccmxy <windrainicefire@gmail.com>
2015-09-18 08:53:06 -0700

    all tests passing, site works

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,27 @@
 
-var bottlesOfBeer = function(number) {
-  var temp = ("\n" + number + " bottles of beer on the wall, " +
-  number + " bottles of beeer.... take one down, " +
-  "pass it around,\n" + (number - 1) +
-  " bottles of beer on the wall!");
-  temp = temp
-  if(number < 1){
-    return 1;
-  }
-  /*
-  temp = (temp + bottlesOfBeer(number - 1));
-  */
-  return temp;
-};
+//function which is not used except for testing because I don't know how to get the
+// newlines to print in the html file, from a string in a js file:
+var bottlesOfBeerForScripts = function(number) {
+    var temp = ("\n" + number + " bottles of beer on the wall, " +
+    number + " bottles of beeer.... take one down, " +
+    "pass it around,\n" + (number - 1) +
+    " bottles of beer on the wall!");
+    if(number < 1){
+      return ("No more bottles of beer on the wall," +
+       " no more bottles of beer." +
+       " Go to the store and buy some more,"  +
+     "  99 bottles of beer on the wall.");
+    }
+    temp = (temp + bottlesOfBeerForScripts(number - 1));
+    return temp;
+  };
 
 
 $(document).ready(function() {
-  $("form#get-fact").submit(function(event) {
-    var number = parseInt($("input#number").val());
-  //  var factorial = getFactorial(number);
-    var bottles = bottlesOfBeer(number);
-  /*  for(var i = 0; i < number; i++){
-      bottles = bottlesOfBeer(i);
-      $(".bottles").text(bottles);
-    }*/
-
+  $("form#get-bottles").submit(function(event) {
+   var number = parseInt($("input#number").val());
+   //Calling the function in the file take-one-down.html:
+   bottlesOfBeer(number);
     event.preventDefault();
   });
 });
